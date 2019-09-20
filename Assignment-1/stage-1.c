38ce3fe241eb30074811ef6b9d77c65f81b90a87
verbal-noun <kaifahsan420@gmail.com>
2019-09-21 00:37:47 +1000

    stage one done except indentation

--- Assignment-1/stage-1.c
+++ Assignment-1/stage-1.c
@@ -1,49 +1,61 @@
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 
 #define MAXLEN 999
 
-
 int main(int argc, char const *argv[])
 {
-    char buffer[MAXLEN];
-    char *b = buffer; 
+
+    // Very suspicicous... Just use buffer as a char*. Don't declare an array and 'point' b to that array
+    //char buffer[MAXLEN];
+    char *buffer;
     size_t buffer_size = MAXLEN;
-    const char s[10] = ", -"; 
+    const char s[10] = " '\t''\n'";
     char* tok;
-    char *str[MAXLEN];
-    int i = 0;
-    
-    while (getline(&b, &buffer_size, stdin) != EOF)
+    // Changed str to 2d array. Maybe 'words' is a more appropriate name
+    char words[1000][MAXLEN];
+    int numWords = 0;
+    int count = 0;
+    while (getline(&buffer, &buffer_size, stdin) != EOF)
     {
-        char line[] = "";
-    
-        // tokenising the input 
+        //char line[] = "";
+        // For every line we read in, lets see what we read in
+        // tokenising the input
         tok = strtok(buffer, s);
+        printf("%c\n", buffer[0]);
+        //For every new line we should reset numWords to 0
+        if(buffer[0] == '.') {
+            //
+        }
+        else {
+            numWords = 0;
+            while (tok != NULL) {
+            // Here we have our tokens coming in. Save these words into our array
+            strcpy(words[numWords++], tok);
 
-        while (tok != NULL) { 
-        str[i++] = tok;
-        // Use of strtok 
-        // go through other tokens 
-        tok = strtok(NULL, s); 
-        } 
-
-        for(int j=0; j <i; j++) 
-        {
-            if(strlen(line) == 0) {
-                strcat(line, str[j]);
-            } else
-            {
-                strcat(line, " ");
-                strcat(line, str[j]);
-
+            // Use of strtok
+            // go through other tokens
+            tok = strtok(NULL, s);
             }
         }
-        tok = strtok(NULL, s); 
+        
 
-        printf("%s", line);
+        // Here, we should have copied all the words into words.
+
+        // Lets print out everything in words, for each of our line read
+        /*
+        for(int i = 0; i < numWords; i ++){
+            count = count + strlen(words[i]) + 1;
+            if(count < 51) {
+                printf("%s ", words[i]);
+            } else {
+                printf("\n");
+                count = 0;
+            }
+            
+        } */
     }
-    
-    
+
+
     return 0;
 }
--- Assignment-1/test.c
+++ Assignment-1/test.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 
 #define MAXLEN 999
-#define LIMIT 50
-
-int getlines(char line[], int limit);
 
 int main(int argc, char const *argv[])
 {
-    char str[999];
-    char ch;
-    while((ch = getchar()) != EOF) {
-        getlines(str, LIMIT);
-        puts(str);
-    }
-    
-    
-    return 0;
-}
 
+    // Very suspicicous... Just use buffer as a char*. Don't declare an array and 'point' b to that array
+    //char buffer[MAXLEN];
+    char *buffer;
+    size_t buffer_size = MAXLEN;
+    const char s[10] = ", -";
+    char* tok;
+    // Changed str to 2d array. Maybe 'words' is a more appropriate name
+    char words[1000][MAXLEN];
+    int numWords = 0;
+    while (getline(&buffer, &buffer_size, stdin) != EOF)
+    {
+        //char line[] = "";
+        // For every line we read in, lets see what we read in
+        printf("Line read: %s ", buffer);
 
-int getlines(char line[], int limit)
-{
-    int c, len = 0;
-    while((c = getchar()) != EOF && c != '\n' && len < limit) {
-        line[len] = c;
-        len++;
+        // tokenising the input
+        tok = strtok(buffer, s);
+
+        //For every new line we should reset numWords to 0
+        numWords = 0;
+        while (tok != NULL) {
+        // Here we have our tokens coming in. Save these words into our array
+        strcpy(words[numWords++], tok);
+
+        // Use of strtok
+        // go through other tokens
+        tok = strtok(NULL, s);
+        }
+        
+        // Here, we should have copied all the words into words.
+
+        // Lets print out everything in words, for each of our line read
+        printf("Tokenised line: ");
+        for(int i = 0; i < numWords; i ++){
+            printf("%s ", words[i]);
+        }
+
+        printf("\n");
     }
-    line[len] = '\0';
-    if(c == EOF) {
-        return EOF;
-    }
+
 
     return 0;
 }
